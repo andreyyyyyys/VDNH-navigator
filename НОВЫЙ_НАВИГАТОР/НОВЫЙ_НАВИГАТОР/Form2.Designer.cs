112ea8c90a0b5db32a5effbead58b8d289dde9e2
fgfgfgfgfgfggf <125589917+fgfgfgfgfgfggf@users.noreply.github.com>
2023-02-17 17:53:50 +0300

    Add files via upload

---
+++ НОВЫЙ_НАВИГАТОР/НОВЫЙ_НАВИГАТОР/Form2.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace НОВЫЙ_НАВИГАТОР
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.OBSHIU_TAIMER = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(894, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(181, 673);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(25, 92);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 38);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "выбрать на карте конец маршрута";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "выбрать на карте точку отправления";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(6, 19);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(169, 33);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Построить маршрут от одной точки до другой:";
+            // 
+            // OBSHIU_TAIMER
+            // 
+            this.OBSHIU_TAIMER.Enabled = true;
+            this.OBSHIU_TAIMER.Tick += new System.EventHandler(this.OBSHIU_TAIMER_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::НОВЫЙ_НАВИГАТОР.Properties.Resources.тестовая_контрольная_точка1;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(317, 207);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(15, 14);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::НОВЫЙ_НАВИГАТОР.Properties.Resources.тестовая_контрольная_точка1;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(414, 207);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(15, 14);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = global::НОВЫЙ_НАВИГАТОР.Properties.Resources.тестовая_контрольная_точка1;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(435, 207);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(15, 14);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImage = global::НОВЫЙ_НАВИГАТОР.Properties.Resources.тестовая_контрольная_точка1;
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Location = new System.Drawing.Point(435, 275);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(15, 14);
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackgroundImage = global::НОВЫЙ_НАВИГАТОР.Properties.Resources.тестовая_контрольная_точка1;
+            this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox5.Location = new System.Drawing.Point(317, 275);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(15, 14);
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackgroundImage = global::НОВЫЙ_НАВИГАТОР.Properties.Resources.тестовая_контрольная_точка1;
+            this.pictureBox6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox6.Location = new System.Drawing.Point(529, 207);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(15, 14);
+            this.pictureBox6.TabIndex = 6;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackgroundImage = global::НОВЫЙ_НАВИГАТОР.Properties.Resources.тестовая_контрольная_точка1;
+            this.pictureBox7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox7.Location = new System.Drawing.Point(529, 275);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(15, 14);
+            this.pictureBox7.TabIndex = 7;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BackgroundImage = global::НОВЫЙ_НАВИГАТОР.Properties.Resources.тестовая_контрольная_точка1;
+            this.pictureBox8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox8.Location = new System.Drawing.Point(320, 139);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(15, 14);
+            this.pictureBox8.TabIndex = 8;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.BackgroundImage = global::НОВЫЙ_НАВИГАТОР.Properties.Resources.тестовая_контрольная_точка1;
+            this.pictureBox9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox9.Location = new System.Drawing.Point(258, 207);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(15, 14);
+            this.pictureBox9.TabIndex = 9;
+            this.pictureBox9.TabStop = false;
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.BackgroundImage = global::НОВЫЙ_НАВИГАТОР.Properties.Resources.тестовая_контрольная_точка1;
+            this.pictureBox10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox10.Location = new System.Drawing.Point(595, 224);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(15, 14);
+            this.pictureBox10.TabIndex = 10;
+            this.pictureBox10.TabStop = false;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::НОВЫЙ_НАВИГАТОР.Properties.Resources.новая_карта;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1070, 675);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form2_MouseClick);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form2_MouseDown);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer OBSHIU_TAIMER;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox10;
+    }
+}
---
+++ НОВЫЙ_НАВИГАТОР/НОВЫЙ_НАВИГАТОР/Form2.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace НОВЫЙ_НАВИГАТОР
+{
+    public partial class Form2 : Form
+    {
+        
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public int VbIBR_TOCHKI_NACHALA_MARSHRYTA_X = 0; //
+        public int VbIBR_TOCHKI_NACHALA_MARSHRYTA_Y = 0; //     координаты начала и конца маршрута
+        public int VbIBR_TOCHKI_KONCA_MARSHRYTA_X = 0;   //
+        public int VbIBR_TOCHKI_KONCA_MARSHRYTA_Y = 0;   //
+
+        bool JDATb_NACHALA_MARSHRYTA = false;    //нажата кнопка выбора начала маршрута или нет
+        bool JDATb_KONCA_MARSHRYTA = false;      //нажата кнопка выбора конца маршрута или нет
+        bool JDATb_POSTRONIU_MARSHRYTA = false;  //начальная и конечная точка маршрута выбраны, ожидание построения
+
+        PictureBox[] TOCHKI = new PictureBox[12];   //массив контрольных точек
+
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            TOCHKI[0]=pictureBox1;
+            TOCHKI[1]=pictureBox2;
+            TOCHKI[2]=pictureBox3;
+            TOCHKI[3]=pictureBox4;
+            TOCHKI[4]=pictureBox5;
+            TOCHKI[5]=pictureBox6;
+            TOCHKI[6]=pictureBox7;
+            TOCHKI[7]=pictureBox8;
+            TOCHKI[8]=pictureBox9;
+            TOCHKI[9]=pictureBox10;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            JDATb_NACHALA_MARSHRYTA = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            JDATb_KONCA_MARSHRYTA=true;
+        }
+
+        private void Form2_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form2_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (JDATb_NACHALA_MARSHRYTA==true)
+            {
+                VbIBR_TOCHKI_NACHALA_MARSHRYTA_X=e.Location.X;
+                VbIBR_TOCHKI_NACHALA_MARSHRYTA_Y=e.Location.Y; //создание точки начала маршрута на карте
+                Button NACHALO = new Button();
+                NACHALO.Location=new Point(VbIBR_TOCHKI_NACHALA_MARSHRYTA_X, VbIBR_TOCHKI_NACHALA_MARSHRYTA_Y);
+                NACHALO.Size=new Size(15, 15);
+                NACHALO.BackColor=Color.Purple;
+                NACHALO.Enabled=false;
+                this.Controls.Add(NACHALO);
+                JDATb_NACHALA_MARSHRYTA=false;
+            }
+            if (JDATb_KONCA_MARSHRYTA==true)
+            {
+                VbIBR_TOCHKI_KONCA_MARSHRYTA_X=e.Location.X;
+                VbIBR_TOCHKI_KONCA_MARSHRYTA_Y=e.Location.Y;  //создание точки конца маршрута на карте
+                Button KONEC = new Button();
+                KONEC.Location=new Point(VbIBR_TOCHKI_KONCA_MARSHRYTA_X, VbIBR_TOCHKI_KONCA_MARSHRYTA_Y);
+                KONEC.Size=new Size(15, 15);
+                KONEC.BackColor=Color.Purple;
+                KONEC.Enabled=false;
+                this.Controls.Add(KONEC);
+                JDATb_KONCA_MARSHRYTA=false;
+                JDATb_POSTRONIU_MARSHRYTA=true;
+
+            }
+        }
+
+        private void OBSHIU_TAIMER_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+//for i in range(10):
+//    print("            TOCHKI[", i, "]=pictureBox", i+1, ";", sep='')
